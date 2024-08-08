6d5eabcf3f2c4543408668f9e47c44ec419a5405
Gustavo Sutana <gustavosutanalima@gmail.com>
2024-08-08 13:33:58 -0300

    update na Classe SmartTV.java, adiciona comentários relevantes a escopo de variáveis.

--- src/edu/gustavo/metodos/SmartTV.java
+++ src/edu/gustavo/metodos/SmartTV.java
@@ -2,10 +2,14 @@ package edu.gustavo.metodos;
 
 public class SmartTV {
 
-    //definindo os atributos da classe.:
-    public int canal = 0;
-    public boolean ligada = false;
-    public int volume = 0;
+    //definindo os atributos da classe, ou VARIÁVEIS DE ESCOPO DE CLASSE:
+                                         //VARIÁVEIS DE ESCOPO DE CLASSE PODEM SER UTILIZADA POR TODOS OS METODOS DA CLASSE;
+                                         //NO ENTANTO, VARIÁVEIS DE ESCOPO DE MÉTODO SÓ PODEM SER ACESSADAS POR DENTRO DO
+                                         //MÉTODO EM QUE ELA FOI DECLARADA;
+
+    public int canal = 0; //VARIÁVEL DE CLASSE;
+    public boolean ligada = false; //VARIÁVEL DE CLASSE;
+    public int volume = 0; //VARIÁVEL DE CLASSE;
   
     
     //metodo ligar.: (throws Exception quer dizer que, caso o usuário tente ligar uma tv ligada, não fará sentido, por isso é necessário 
@@ -42,7 +46,8 @@ public class SmartTV {
     }
 
     public void mudarCanalEspecífico (int numeroCanal) {
-        canal = numeroCanal;
+        canal = numeroCanal; //VARIÁVEL DE MÉTODO, SÓ PODE SER ACESSADA DENTRO DESSE MÉTODO, NÃO É POSSÍVEL USÁ-LA EM QUALQUER OUTRO 
+                             //MÉTODO DESTA CLASSE;
     }
 
     public void aumentarCanal() {
